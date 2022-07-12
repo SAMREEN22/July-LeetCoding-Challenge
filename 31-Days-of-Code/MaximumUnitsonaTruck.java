b1b34e70aaea828110aec69c81f07530b25a22d5
SAMREEN SIDDIQUI <samreen.1923it1191@kiet.edu>
2022-07-12 21:46:18 +0530

    Day1

--- 31-Days-of-Code/MaximumUnitsonaTruck.java
+++ 31-Days-of-Code/MaximumUnitsonaTruck.java
@@ -1,7 +1,6 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         int n = boxTypes.length;
-        
         Arrays.sort(boxTypes,(a,b)->b[1]-a[1]);
         int ans=0;
         for(int i=0;i<n;i++){
