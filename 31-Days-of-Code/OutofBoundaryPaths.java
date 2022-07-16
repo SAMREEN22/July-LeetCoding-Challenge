4b0ca43bb089f1668def19d737d5700bd25a2267
SAMREEN SIDDIQUI <samreen.1923it1191@kiet.edu>
2022-07-16 14:16:55 +0530

    Day16

---
+++ 31-Days-of-Code/OutofBoundaryPaths.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int mod=1000000007;
+        int dp[][]=new int[m][n];
+        int dp1[][]=new int[m][n];
+        int count=0;
+        int dir[][]=new int[][]{{1,0},{-1,0},{0,1},{0,-1}}; 
+        dp[startRow][startColumn]=1;
+        for(int mo=1;mo<=maxMove;mo++){ 
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(dp[i][j]!=0){  
+                    for(int k=0;k<4;k++){ 
+                        int x=i+dir[k][0];
+                        int y=j+dir[k][1];
+                        if(x<0 || x>=m || y<0 || y>=n){ 
+                            count=(count+dp[i][j])%mod;
+                        }
+                        else{
+                            dp1[x][y]=(dp1[x][y]+dp[i][j])%mod;
+                        }
+                    }
+                  }
+                }
+            }
+            dp=dp1; 
+            dp1=new int[m][n];
+        }
+        return count;    
+    }
+}
+
+
+
+//Take two 2D array
+//isit the first array and update the value to the next array.
+//If the indexes go out of the boundary increment the count.
+//Else update the total no. of visit of the cell in the next array.
+//Now, evaluate the next array and declare another fresh new array.
